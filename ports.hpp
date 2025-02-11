af61d5060e5b15180caee0b0065c8357363f2003
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-11 13:34:16 -0500

    Create ports.hpp

---
+++ ports.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "main.h"
+
+// ===================== Devices ===================== //
+
+//main devices
+extern brain Brain;
+extern controller controller1;
+
+//motors
+
+//pods
+
+//extra
