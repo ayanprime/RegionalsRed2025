934a53472c53243c15af790249622619bcc6f4ea
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-19 07:58:08 -0500

    Update ports.cpp

--- ports.cpp
+++ ports.cpp
@@ -2,29 +2,16 @@
 
 // ===================== Devices ===================== //
 
-//main devices
+// main devices
 brain Brain;
 controller controller1 = controller(primary);
 
-//motors
-motor rmA = motor(PORT9, ratio18_1, true);
-motor rmB = motor(PORT10, ratio18_1, true);
-motor_group Right = motor_group(rmA, rmB);
-
-motor lmA = motor(PORT17, ratio18_1, false);
-motor lmB = motor(PORT18, ratio18_1, false);
-motor_group Left = motor_group(lmA, lmB);
-
-//pods
-rotation RightP = rotation(PORT8, false);
-
-rotation LeftP = rotation(PORT20, true);
+//conveyor
+motor conveyor = motor(PORT9, ratio6_1, false);
 
 //intake
-motor intake = motor(PORT6, ratio18_1, true);
+motor intake = motor(PORT10, ratio18_1, true);
 
-//conveyor
-motor conveyor = motor(PORT11, ratio18_1, true);
-
-//clamp
-digital_out latch = digital_out(Brain.ThreeWirePort.H);
+//wall-stake scorer
+motor leftWallStake = motor(PORT1, ratio18_1, false);
+motor rightWallStake = motor(PORT2, ratio18_1, true);
