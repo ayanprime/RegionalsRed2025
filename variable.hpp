0c32c97eeb70b23e5eb593d6d5777f8533c90cee
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-19 07:59:46 -0500

    Update variable.hpp

--- variable.hpp
+++ variable.hpp
@@ -1,36 +1,7 @@
 #pragma once
 #include "main.h"
 
-// Declare global variables using extern
-extern vex::timer myTimer;
-extern double dt;
-extern double odomR;
-extern double odomL;
-extern double etR;
-extern double etL;
-extern double RKp;
-extern double LKp;
-extern double dis;
-extern double disR;
-extern double disL;
-extern double angR;
-extern double angL;
-extern double IoR;
-extern double IoL;
-extern int elapsedTime;
-extern float Ki;
-extern double PR;
-extern double PL;
-extern double IR;
-extern double IL;
-extern double pi;
-extern bool check;
-extern bool moveone;
-extern bool movetwo;
-extern bool movethree;
-extern bool movefour;
-extern bool R;
-extern bool L;
-extern bool Ayan;
-extern bool Bennett;
-extern bool Max;
+// ===================== Variables ===================== //
+
+// wall-stake position
+extern int wallStakePosition;
