86c63e478fce34ec931ce41ad88cfb7b6ba68b07
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-24 08:14:11 -0500

    Update variable.hpp

--- variable.hpp
+++ variable.hpp
@@ -5,3 +5,21 @@
 
 // wall-stake position
 extern int wallStakePosition;
+
+// side selection
+extern char side;
+
+// p-control variables
+extern float PR;
+extern float PL;
+extern float RKp;
+extern float LKp;
+extern float etR;
+extern float etL;
+extern float odomR;
+extern float odomL;
+
+// timer variables
+extern float startTime;
+extern float endTime;
+extern bool stop;
