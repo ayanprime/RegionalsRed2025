8473f4a92067e01955dfa0b10c13aa7ed303f657
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-24 08:12:17 -0500

    Update ports.cpp

--- ports.cpp
+++ ports.cpp
@@ -7,11 +7,33 @@ brain Brain;
 controller controller1 = controller(primary);
 
 //conveyor
-motor conveyor = motor(PORT9, ratio6_1, false);
+motor conveyor = motor(PORT2, ratio6_1, false);
 
 //intake
-motor intake = motor(PORT10, ratio18_1, true);
+motor intake = motor(PORT1, ratio18_1, true);
 
 //wall-stake scorer
-motor leftWallStake = motor(PORT1, ratio18_1, false);
-motor rightWallStake = motor(PORT2, ratio18_1, true);
+motor leftWallStake = motor(PORT18, ratio36_1, false);
+motor rightWallStake = motor(PORT8, ratio36_1, true);
+
+//drive-train
+motor LF = motor(PORT13, ratio18_1, false);
+motor LB = motor(PORT9, ratio18_1, false);
+motor_group LeftDrive(LF, LB);
+
+motor RF = motor(PORT10, ratio18_1, true);
+motor RB = motor(PORT15, ratio18_1, true);
+motor_group RightDrive(RF, RB);
+
+//pods
+rotation leftPod = rotation(PORT12, true);
+rotation rightPod = rotation(PORT11, false);
+
+//sorter
+optical sorter = optical(PORT19);
+
+//clamp
+digital_out clamp = digital_out(Brain.ThreeWirePort.F);
+
+//selector
+pot selector = pot(Brain.ThreeWirePort.A);
