da46f9684c2846581db0d85a3f8665c1d5051abd
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-24 08:13:11 -0500

    Update ports.hpp

--- ports.hpp
+++ ports.hpp
@@ -16,3 +16,25 @@ extern motor intake;
 //wall-stake scorer
 extern motor leftWallStake;
 extern motor rightWallStake;
+
+//drive-train
+extern motor LF;
+extern motor LB;        
+extern motor_group LeftDrive;
+
+extern motor RF;
+extern motor RB;
+extern motor_group RightDrive;
+
+//pods
+extern rotation leftPod;
+extern rotation rightPod;
+
+//sorter
+extern optical sorter;
+
+//clamp
+extern digital_out clamp;
+
+//selector
+extern pot selector;
