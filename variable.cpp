75c0f9ac6a382af56583fbb201cdc912eddc1870
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-02-24 08:13:52 -0500

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -4,3 +4,21 @@
 
 // wall-stake position
 int wallStakePosition = 0;
+
+// side selection
+char side;
+
+// p-control variables
+float PR;
+float PL;
+float RKp = 1.5;
+float LKp = 1.5;
+float etR;
+float etL;
+float odomR;
+float odomL;
+
+// timer variables
+float startTime;
+float endTime;
+bool stop = false;
